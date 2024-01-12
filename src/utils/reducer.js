0ea593e917c90760183d728cc95b749e661702ec
Bekzodjan <bekzod.dasturchi.2010@gmail.com>
2024-01-12 16:34:24 +0500

    less3

--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -1,65 +1,35 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 
 function reducer(state, { type, payload }) {
   switch (type) {
-    case "change":
-      state.taskObj = { ...state.taskObj, name: payload, active: true };
-      break;
-    case "add":
-      if (state.current === "") {
-        state.tasks.push(state.taskObj);
-      } else {
-        state.tasks[state.current] = state.taskObj;
-      }
-      state.current = "";
-      state.tasks = [...state.tasks];
-      break;
-    case "changeStatus":
-      state.tasks[payload].active = !state.tasks[payload].active;
-      // state.delTasks.push(...state.tasks.splice(payload,1))
-      state.tasks.filter((itm, i) => {
-        if (!itm.active) {
-          state.delTasks.push(...state.tasks.splice(i, 1));
-        }
-      });
-      state.tasks = [...state.tasks];
-      state.delTasks = [...state.delTasks];
-      break;
-    case "changeStatus2":
-      state.delTasks[payload].active = !state.delTasks[payload].active;
-      // state.delTasks.push(...state.tasks.splice(payload,1))
-      state.delTasks.filter((itm, i) => {
-        if (itm.active) {
-          state.tasks.push(...state.delTasks.splice(i, 1));
-        }
-      });
-      state.tasks = [...state.tasks];
-      state.delTasks = [...state.delTasks];
-      break;
-    case "clear":
-      state.tasks.map((itm) => ({ ...itm, active: !itm.active }));
-      state.delTasks.push(...state.tasks.splice(0));
-      state.tasks = [...state.tasks];
-      state.delTasks = [...state.delTasks];
-      break;
-    case "delete":
-      state.delTasks.splice(0);
-      state.tasks = [...state.tasks];
-      state.delTasks = [...state.delTasks];
-      break;
-    case "edit":
-      state.taskObj = { ...state.taskObj, name: state.tasks[payload].name };
-      state.current = payload;
+    case 'plus':
+      state.count++
+      break
+    case 'minus':
+      state.count--
+      break
   }
 
   return { ...state };
 }
 
+const boboF = (store)=>(next)=>(actions)=>{
+  console.log(store.getState());
+  console.log(actions);
+  next(actions)
+  console.log(store.getState());
+  console.log('______________');
+}
+
 const store = createStore(reducer, {
-  taskObj: {},
-  tasks: [],
-  delTasks: [],
-  current: "",
-});
+  count: 0
+},applyMiddleware());
+
+store.dispatch({type: "plus"})
+store.dispatch({type: "plus"})
+store.dispatch({type: "plus"})
+store.dispatch({type: "plus"})
+store.dispatch({type: "plus"})
+store.dispatch({type: "minus"})
 
 export default store;
